ade24333d7b7ece1c0defeebaf691ddbf21af2dd
Salavat Zabirov <zab.sal42@gmail.com>
2018-03-14 13:18:53 +0300

    Renamed

---
+++ Lesson01/src/com/company/CharacterExample.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class CharacterExample {
+    public static void main(String[] args) {
+        char c1 = 65;
+        char c2 = 66;
+        char c3 = (char) (c1 + c2);
+        System.out.println(c3);
+    }
+}
---
+++ Lesson03/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	    int a = 7;
+	    int c = 5;
+	    //пример переменной типа boolean
+		//b примет значение true, если a меньше c
+		//b примет значение false, если a больше либор равно c
+	    boolean b = a < c;
+    }
+}
