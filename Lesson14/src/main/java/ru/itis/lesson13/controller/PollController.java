60b92149110b5b40dd0e103d58edab00651abaa8
SZabirov <zab.sal42@gmail.com>
2018-04-18 18:54:32 +0300

    Poll. JdbcTemplate

---
+++ Lesson14/src/main/java/ru/itis/lesson13/controller/PollController.java
@@ -0,0 +1,31 @@
+package ru.itis.lesson13.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.itis.lesson13.dao.AnswerDao;
+import ru.itis.lesson13.dao.PollDao;
+import ru.itis.lesson13.model.Answer;
+import ru.itis.lesson13.model.Poll;
+
+import java.util.List;
+
+@RestController
+public class PollController {
+    PollDao pollDao;
+    @Autowired
+    AnswerDao answerDao;
+
+    @RequestMapping("/polls/{id}")
+    Poll getPollById(@PathVariable Long id) {
+        return pollDao.findById(id);
+    }
+
+    @RequestMapping("/answers")
+    List<Answer> getAnswersByPollId(@RequestParam Long pollId) {
+        return answerDao.getAllByPollId(pollId);
+    }
+}
